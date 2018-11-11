e08a24e88c8f0cdc734267461b389177c38e2767
Aaron Nassiry <anassiry@salesforce.com>
2018-11-10 22:29:53 -0800

    test

--- JavaPracticesNov2018/src/enume/Enum.java
+++ JavaPracticesNov2018/src/enume/Enum.java
@@ -2,6 +2,9 @@ package enume;
 public class Enum {
 public enum Season {spring, summer, fall, winter};
     public static void main(String[] args) {
-        System.out.println(Season.fall);
+    	System.out.println(Season.spring);
+    	System.out.println(Season.summer);
+    	System.out.println(Season.fall);
+    	System.out.println(Season.winter);
     }    
 }
