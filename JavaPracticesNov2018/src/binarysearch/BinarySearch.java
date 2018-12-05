2f579ab0152dc94c110f5a22e4da1046463d1cf7
Aaron Nassiry <anassiry@salesforce.com>
2018-12-04 19:23:42 -0800

    binary seasrch

---
+++ JavaPracticesNov2018/src/binarysearch/BinarySearch.java
@@ -0,0 +1,36 @@
+package binarysearch;
+
+public class BinarySearch {
+
+	public static int leftIndex;
+	public static int rightIndex;
+
+	public static int  binarySearch(int arr[], int x) 
+    { 
+		rightIndex = arr.length - 1; 
+        while (leftIndex <= rightIndex) 
+        { 
+            int m = leftIndex + (rightIndex - leftIndex)/2; 
+            if (arr[m] == x) 
+                return m; 
+  
+            if (arr[m] < x) 
+            	leftIndex = m + 1; 
+  
+            else
+            	rightIndex = m - 1; 
+        } 
+        return -1; 
+    } 
+
+	public static void main(String args[]) {
+		int arr[] = { 1, 2, 3, 4, 5, 10, 40 };
+		int x = 10;
+
+		int result = BinarySearch.binarySearch(arr, x);
+		if (result == -1)
+			System.out.println("Element not present");
+		else
+			System.out.println("Element found at " + "index " + result);
+	}
+}
