92b6347f769eb5b7122d3ae6782a4f47167c1c82
Aaron Nassiry <anassiry@salesforce.com>
2020-10-04 22:38:25 -0700

    add test files

---
+++ Java_8_Practice/src/SubClass.java
@@ -0,0 +1,11 @@
+class SuperClass {
+    SuperClass(int x) {
+        System.out.println("Super");
+    }
+}
+public class SubClass extends SuperClass {
+    SubClass() {
+        super(5);
+        System.out.println("Sub 2");
+    }
+}
